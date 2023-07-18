730627d8cb763c17f9f2578c94f3f76af3a34673
Irina G <irina.golionko@gmail.com>
2023-07-17 22:36:49 -0400

    add Form child component of Appointment and stories for it

---
+++ src/components/Appointment/Form.js
@@ -0,0 +1,68 @@
+/*
+The <Form> component should track the following state:
+
+student:String
+interviewer:Number
+The <Form> component should have the following actions:
+
+setStudent:Function
+setInterviewer:Function
+The <Form> component should take the following props:
+
+student:String
+interviewers:Array
+interviewer:Number
+onSave:Function
+onCancel:Function
+*/
+
+import React, { useState } from "react";
+
+import InterviewerList from "components/InterviewerList";
+import Button from "components/Button";
+
+export default function Form(props) {
+  const reset = () => {
+    setStudent("");
+    setInterviewer(null);
+  };
+
+  const cancel = () => {
+    reset();
+    props.onCancel();
+  };
+
+  const [student, setStudent] = useState(props.student || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  return (
+    <main className="appointment__card appointment__card--create">
+      <section className="appointment__card-left">
+        <form autoComplete="off" onSubmit={(event) => event.preventDefault()}>
+          <input
+            className="appointment__create-input text--semi-bold"
+            name="name"
+            type="text"
+            placeholder="Enter Student Name"
+            value={student}
+            onChange={(event) => setStudent(event.target.value)}
+          />
+        </form>
+        <InterviewerList
+          interviewers={props.interviewers}
+          value={interviewer}
+          onChange={setInterviewer}
+        />
+      </section>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <Button danger onClick={cancel}>
+            Cancel
+          </Button>
+          <Button confirm onClick={() => props.onSave(student, interviewer)}>
+            Save
+          </Button>
+        </section>
+      </section>
+    </main>
+  );
+}
