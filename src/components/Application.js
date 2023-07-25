bea883ba7066cee8858441c9e431e6bee79e7e2e
Irina G <irina.golionko@gmail.com>
2023-07-25 16:29:32 -0400

    fetch intreview data and present the interviewer name

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,7 +4,7 @@ import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
 import axios from "axios";
-import { getAppointmentsForDay } from "helpers/selectors";
+import { getAppointmentsForDay, getInterview } from "helpers/selectors";
 
 export default function Application() {
   // conbined state of the application
@@ -12,6 +12,7 @@ export default function Application() {
     day: "Monday",
     days: [],
     appointments: {},
+    interviewers: {},
   });
 
   // appointments for a specific day that is currently in our state
@@ -22,16 +23,31 @@ export default function Application() {
 
   // API calls to get the days and appointments & update the state after both have returned
   useEffect(() => {
-    Promise.all([axios.get(`/api/days`), axios.get(`/api/appointments`)]).then((all) => {
-      console.log("days", all[0].data);
-      console.log("appointments", all[1].data);
-      setState((prev) => ({ ...prev, days: all[0].data, appointments: all[1].data }));
+    Promise.all([
+      axios.get(`/api/days`),
+      axios.get(`/api/appointments`),
+      axios.get(`/api/interviewers`),
+    ]).then((all) => {
+      setState((prev) => ({
+        ...prev,
+        days: all[0].data,
+        appointments: all[1].data,
+        interviewers: all[2].data,
+      }));
     });
   }, []);
 
   // list of Appointment components created based on the dailyAppointments
   const appointmentList = dailyAppointments.map((appointment) => {
-    return <Appointment key={appointment.id} {...appointment} />;
+    const interview = getInterview(state, appointment.interview);
+    return (
+      <Appointment
+        key={appointment.id}
+        id={appointment.id}
+        interview={interview}
+        time={appointment.time}
+      />
+    );
   });
 
   // last Appointment of the day that cannot be booked
