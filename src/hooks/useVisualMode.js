8b447b7d7db72aaae941079933049f31595bec78
Irina G <irina.golionko@gmail.com>
2023-07-26 21:57:50 -0400

    add useVisualMode custom hook, add tests

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -4,12 +4,23 @@ export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
 
-  const transition = (newMode) => {
-    setHistory((prev) => [...prev, newMode]);
+  const transition = (newMode, transition = false) => {
+    if (transition) {
+      //if transition is true, remove from history the last mode, and add the new one.
+      setHistory((prev) => {
+        const prevRemovedLast = [...prev].slice(0, -1);
+        return [...prevRemovedLast, newMode];
+      });
+    } else {
+      setHistory((prev) => [...prev, newMode]);
+    }
+
+    // set the new mode
     setMode(newMode);
   };
 
   const back = () => {
+    // if history has more modes apart of the initial one, allow to go back
     if (history.length > 1) {
       setHistory([...history].slice(0, -1));
       setMode(history[history.length - 2]);
